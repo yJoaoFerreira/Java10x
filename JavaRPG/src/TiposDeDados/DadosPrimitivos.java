492f54f4c530a528a62488175479d12d365f05da
Joao Ferreira <yferreirajoao@gmail.com>
2025-06-22 22:15:19 -0300

    Update Dados Primitivos

---
+++ JavaRPG/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,19 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    // Atalho: "psvm"
+    public static void main(String[] args) {
+        // Dados Primitivos - int, double, float, char, boolean, short
+        // Objetivo da aula: Criar um personagem de RPG - Haisuu
+
+        // Valor máximo do int: 2 147 483 647
+        int idade = 20;
+        double altura = 1.85;
+        // No char é necessário usar aspas simples
+        char inicial = 'H';
+        boolean vivo = true;
+        boolean morto = false;
+        // Valor máximo do Long: 9 223 372 036 854 775 807
+        Long saldo = 1000000L;
+    }
+}
