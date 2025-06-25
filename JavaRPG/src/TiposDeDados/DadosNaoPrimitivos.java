2b6777167f2a4e5830febd0c7e5779422ed2c741
Joao Ferreira <yferreirajoao@gmail.com>
2025-06-25 14:03:13 -0300

    Output explained

---
+++ JavaRPG/src/TiposDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,7 @@
+package TiposDeDados;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) { // psvm
+
+    }
+}
--- JavaRPG/src/TiposDeDados/DadosPrimitivos.java
+++ JavaRPG/src/TiposDeDados/DadosPrimitivos.java
@@ -15,5 +15,16 @@ public class DadosPrimitivos {
         boolean morto = false;
         // Valor máximo do Long: 9 223 372 036 854 775 807
         Long saldo = 1000000L;
+
+        // Atalho: "sout"
+        // Atalho²: "soutv" ~> Pega a última variável para exibir
+        // Exibir algo quebrando a linha para o usuário
+        System.out.println("Sua idade é: " + idade);
+        System.out.println("Sua altura é: " + altura);
+        System.out.println("A inicial do seu nome é: " + inicial);
+        System.out.println("Você está vivo? " + vivo);
+        System.out.println("Você está morto? " + morto);
+        // Exibir algo para o usuário
+        System.out.print("Seu saldo bancário é de: " + saldo);
     }
 }
